0d807a91a4440f3db34feddcf736d85b9167bd9c
Jaspreet Singh <jaspreet98singh@gmail.com>
2022-10-08 11:48:41 +0530

    capticalise

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
       <div className="flex flex-col items-center justify-around gap-12 py-10 xl:px-16 lg:flex-row rounded-3xl" >
         <div className="max-w-3xl min-w-0" >
           <h4  className="mb-2 lg:mb-3 xl:mb-4 xl:text-[32px] lg:text-xl text-[#F03E3E] font-semibold text-base">About me!</h4>
-          <h3 className="text-[#212529] font-semibold xl:text-5xl lg:text-[34px] text-xl xl:mb-12 lg:mb-6 mb-4">I'm subham Pradhan</h3>
+          <h3 className="text-[#212529] font-semibold xl:text-5xl lg:text-[34px] text-xl xl:mb-12 lg:mb-6 mb-4">I'm Subham Pradhan</h3>
           <p className="xl:leading-[36px] leading-[22px] lg:leading-[24px] text-[#495057] xl:text-xl lg:text-base text-sm">I am Subham Pradhan, the founder and lead developer of Kathas Entertainment.
             <br></br>
             <br></br>
